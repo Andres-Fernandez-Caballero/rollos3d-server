f8f08bf55243ba5ff54df3de3be7f7b76508e6db
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-11-24 17:08:13 -0300

    fix commit 1286463(anterior)

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,13 @@
+
+module.exports =
 {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
+    "dialect": process.env.DATABASE_TYPE
   },
   "test": {
     "username": "root",
